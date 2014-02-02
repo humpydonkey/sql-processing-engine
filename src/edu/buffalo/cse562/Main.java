b20aeb27496a8866e209b49ec3ddfc5c7e518848
Yazhou Cao <yazhouca@buffalo.edu>
2014-02-01 19:58:31 -0500

    just a test for eclipse plugin

--- src/edu/buffalo/cse562/Main.java
+++ src/edu/buffalo/cse562/Main.java
@@ -6,5 +6,5 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("We, the members of our team, agree that we will not submit any code that we have not written ourselves, share our code with anyone outside of our group, or use code that we have not written ourselves as a reference.");
 	}
-
+	
 }
