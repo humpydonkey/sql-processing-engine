3aefde7f037ad9ed9357ea6efa42e7d3032aa50a
Yazhou Cao <yazhouca@buffalo.edu>
2014-03-25 16:18:20 -0400

    a little changes for cp2: structure of data set directory Main function

--- src/edu/buffalo/cse562/Main.java
+++ src/edu/buffalo/cse562/Main.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import net.sf.jsqlparser.parser.CCJSqlParser;
@@ -18,60 +17,72 @@ import dao.Tuple;
 public class Main {
 
 	public static void main(String args[]){
-		String dataDirStr = "data/tpch/";//"data/NBA/";  //"/data/tpch/";
-		String sqlFilePath = "data/cp1_graded_sqls/tpch1.sql";
+		//input example: --data [data] [sqlfile1] [sqlfile2] ...
+		//           or  --data [data] --swap [swap] [sqlfile1] [sqlfile2] ...
 		
-		File dataDir = null;
-		//set arguments
-//        args = new String[]{
-//        		"--data",
-//        		dataDirStr,
-//        		sqlFilePath
-//        };
-
-        ArrayList<File> sqlFiles = new ArrayList<File>();
-        for(int i = 0; i<args.length;i++){
-            if(args[i].equals("--data")){
-                dataDir = new File(args[i+1]);
-                i++;
-            }else {
-                sqlFiles.add(new File(args[i]));
-            }
-        }
-        
-        for (File sql : sqlFiles){
-        	try{
-        		FileReader stream = new FileReader(sql);   		
-        		CCJSqlParser parser = new CCJSqlParser(stream);
-        		Statement stmt;
-
-        		SQLParser myParser = new SQLParser(dataDir);
-        		
-        		while((stmt = parser.Statement()) !=null){		
-        			if(stmt instanceof CreateTable)	
-        				myParser.create(stmt);
-        			else {
-        				
-        			 if(stmt instanceof Select){
-        				//System.out.println("I would now evaluate:" + stmt);
-        				Select sel = (Select)stmt;
-        				List<Tuple> tuples = myParser.select(sel.getSelectBody());
-        				for(Tuple tuple : tuples)
-        					tuple.printTuple();
-        			 }
-        			 else 
-        				System.out.println("PANIC! I don't know how to handle" + stmt);
-        			}
-        		}
-        		
-        	}catch (IOException e){
-        		e.printStackTrace();
-        	}catch (ParseException e){
-        		e.printStackTrace();
-        	}
+		if(args.length>2){
+			if(args[0].equalsIgnoreCase("--data")){
+				String dataDirStr = args[1];
+				File dataDir = new File(dataDirStr);
+				
+				List<File> sqlFiles = new ArrayList<File>();
+				//sqlFiles.add();
+				
+			}else
+				System.out.println("Input error: "+args.toString());
+		}
 		
-	}//end for
-
-  }//end main
+		
+		
+		testAllSQL();
+	}
 	
+	public static void testAllSQL(){
+		//mocking input
+		String dataDirStr = "test/data/";//"data/NBA/";  //"/data/tpch/";
+		String sqlFilePath = "test/cp1_sqls/";
+		
+		String[] args = new String[]{
+        		"--data",
+        		dataDirStr,
+        		"--swap",
+        		sqlFilePath
+        };
+	}
+	
+	public static void runSQL(File dataDir, ArrayList<File> sqlFiles){	        	        
+		for (File sql : sqlFiles){
+			try{
+				FileReader stream = new FileReader(sql);   		
+				CCJSqlParser parser = new CCJSqlParser(stream);
+				Statement stmt;
+		
+				SQLParser myParser = new SQLParser(dataDir);
+				
+				while((stmt = parser.Statement()) !=null){		
+					if(stmt instanceof CreateTable)	
+						myParser.create(stmt);
+					else {
+						
+					 if(stmt instanceof Select){
+						//System.out.println("I would now evaluate:" + stmt);
+						Select sel = (Select)stmt;
+						List<Tuple> tuples = myParser.select(sel.getSelectBody());
+						for(Tuple tuple : tuples)
+							tuple.printTuple();
+					 }
+					 else 
+						System.out.println("PANIC! I don't know how to handle" + stmt);
+		    			}
+		    		}
+		    		
+		    	}catch (IOException e){
+		    		e.printStackTrace();
+		    	}catch (ParseException e){
+		    		e.printStackTrace();
+		    	}
+			
+		}//end for
+	}
+
 }
