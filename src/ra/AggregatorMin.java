39091ef0ed01a164b3e9882b7aeb5c4cfb7850ae
MixC92 <mixcdai@gmail.com>
2014-03-29 16:55:12 -0400

    Add "Max" and "Min"
    Former-commit-id: 000993f4d6567bf298c79240cb098952d8b3e9e9 [formerly 79ef483f631b88d1f26705eb5164dd5399bf6fe0]
    Former-commit-id: 5331ab47ccf2647f2641d131f976ed3960402160

---
+++ src/ra/AggregatorMin.java
@@ -0,0 +1,57 @@
+package ra;
+
+/**
+ * Created by DC on 3/29/14.
+ */
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.Function;
+import dao.Datum;
+import dao.Tuple;
+
+
+public class AggregatorMin extends Aggregator {
+
+    private Map<String, Datum> minMap;
+    private Expression paraExpr;
+
+    public AggregatorMin(Function funcIn, String[] groupByNamesIn) {
+        super(funcIn, groupByNamesIn);
+
+        minMap = new HashMap<String, Datum>();
+
+        @SuppressWarnings("unchecked")
+        List<Expression> paraList = funcIn.getParameters().getExpressions();
+        if (paraList.size() > 1)
+            throw new UnsupportedOperationException("Not supported yet.");
+        else
+            paraExpr = paraList.get(0);
+    }
+
+    @Override
+    public void aggregate(Tuple tuple, String key) {
+        EvaluatorArithmeticExpres eval = new EvaluatorArithmeticExpres(tuple);
+        paraExpr.accept(eval);
+        Datum newVal = eval.getData();
+
+        if (!minMap.containsKey(key)) {
+            //insert new
+            minMap.put(key, newVal);
+        } else {
+            //update old, min
+            Datum oldVal = minMap.get(key);
+            //can not min Bool, String, Date
+            double min = Math.min(oldVal.getNumericValue(), newVal.getNumericValue());
+            oldVal.setNumericValue(min);
+        }
+    }
+
+    @Override
+    public Datum getValue(String key) {
+        return minMap.get(key);
+    }
+}
